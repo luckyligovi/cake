1d6122774b36b31c7d6c4ed56b9ec789af4e6540
luckyligovi <lucky.luvihi@student.moringaschool.com>
2024-09-30 17:38:15 +0300

    about css

--- cake-app/src/components/About.jsx
+++ cake-app/src/components/About.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import cakeTypes from "../image/types-of-cake-18-jpg.jpg";
 
 export default function About() {
   return (
     <div>
       <div className="about-background">
         <h1>ABOUT US</h1>
+      </div>
+      <div className="infor-container">
+        <img
+          src={cakeTypes}
+          alt="cake-types"
+          style={{ width: "300px", height: "auto" ,borderRadius:"20px" }}
+        />
+        <div>
+          <h1>Lorem, ipsum dolor.</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci,
+            repellendus?
+          </p>
+        </div>
       </div>
     </div>
   );
