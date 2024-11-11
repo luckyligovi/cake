383b59ea179d9becd44d30ed2115b0bcbec2cd3f
luckyligovi <lucky.luvihi@student.moringaschool.com>
2024-11-11 13:16:09 +0300

    fix add to cart on category component

--- cake-app/src/components/CakeCategories.jsx
+++ cake-app/src/components/CakeCategories.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import Products from "./Products";
 import Footer from "./Footer";
 
-export default function CakeCategories() {
+export default function CakeCategories({onAddToCart={onAddToCart} }) {
   return (
     <div>
       <div className="main-container">
         <h1>categories</h1>
-        <Products/>
+        <Products onAddToCart={onAddToCart} />
         <Footer/>
       </div>
     </div>
